8776e6edee8f22b08aad6a45bb647d4b6571bc68
IBX00 <ibx.05n@gmail.com>
2022-10-27 13:32:53 +0530

    Imported TMPro package. Testing for strafing data is complete.

--- Assets/Test_Del/TestScript.cs
+++ Assets/Test_Del/TestScript.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-
+    [SerializeField] TextMeshProUGUI angleText;
+    [SerializeField] FloatingJoystick stickLeft, stickRight;
+    
     Camera cam;
 
-    // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
@@ -18,9 +20,16 @@ public class TestScript : MonoBehaviour
         transform.LookAt(Quaternion.Euler(x, y, z) * transform.forward);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 dirLeft = new Vector2(stickLeft.Horizontal, stickLeft.Vertical);
+        Vector2 dirRight = new Vector2(stickRight.Horizontal, stickRight.Vertical);
+
+        float signedAngle = Vector2.SignedAngle(dirRight, dirLeft);
+        float signedAngle2 = Vector2.SignedAngle(dirLeft, dirRight);
+        float unSignedAngle = Vector2.Angle(dirLeft, dirRight);
+        Vector2 result = Quaternion.AngleAxis(signedAngle, Vector3.forward) * new Vector2(0, 1);
+
+        angleText.text = $"Signed: {signedAngle} | {signedAngle2} || Un-Signed: {unSignedAngle} || Result: {result}";
     }
 }
