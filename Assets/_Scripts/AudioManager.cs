d3304a95b6f2df69644799a93bbdd76c18d5bdce
IBX00 <ibx.05n@gmail.com>
2022-12-14 12:21:40 +0530

    Audio Manager complete with audio transitions.

--- Assets/_Scripts/AudioManager.cs
+++ Assets/_Scripts/AudioManager.cs
@@ -11,6 +11,11 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip lobbySceneClip, mpSceneClip;
 
+    [Range(1f, 3f)]
+    [SerializeField] private float transitionSpeed = 2.2f;
+    [SerializeField] private float minVolume, maxVolume;
+    [SerializeField] private float lerpThreshold = 0.05f;
+
     private void Awake()
     {
         if (Instance != null)
@@ -37,16 +42,43 @@ public class AudioManager : MonoBehaviour
 
     private void OnSceneFinishedLoading(Scene scene, LoadSceneMode mode)
     {
+
         int loadedSceneIndex = scene.buildIndex;
         if (loadedSceneIndex == lobbySceneIndex)
         {
-            audioSource.clip = lobbySceneClip;
-            audioSource.Play();
+            StartCoroutine(ChagneAudioToRespectedScene(lobbySceneClip));
         }
         else if (loadedSceneIndex == mpSceneIndex)
         {
-            audioSource.clip = mpSceneClip;
-            audioSource.Play();
+            StartCoroutine(ChagneAudioToRespectedScene(mpSceneClip));
+        }
+    }
+
+    private IEnumerator ChagneAudioToRespectedScene(AudioClip clipSelected)
+    {
+        yield return StartCoroutine(ReduceVolume());
+
+        audioSource.clip = clipSelected;
+        audioSource.Play();
+        
+        yield return StartCoroutine(IncreaseVolume());
+    }
+
+    private IEnumerator ReduceVolume()
+    {
+        while(audioSource.volume > (minVolume + lerpThreshold))
+        {
+            audioSource.volume = Mathf.Lerp(audioSource.volume, minVolume, transitionSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    private IEnumerator IncreaseVolume()
+    {
+        while(audioSource.volume < (maxVolume - lerpThreshold))
+        {
+            audioSource.volume = Mathf.Lerp(audioSource.volume, maxVolume, transitionSpeed * Time.deltaTime);
+            yield return null;
         }
     }
 }
